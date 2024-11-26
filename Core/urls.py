7e27f5dd59d59e955c11563ccfe7e054404c9831
Nezha <esdeath19@gmail.com>
2024-11-26 00:42:38 -0300

    Pestañas

--- Core/urls.py
+++ Core/urls.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 from django.shortcuts import redirect
-from .views import CustomLoginView, Base
+from .views import CustomLoginView, Base,aseo,computacion,profesores
 
 urlpatterns = [
     path('', lambda request: redirect('login/')),  
     path('login/', CustomLoginView.as_view(), name='Login'),
     path('base/', Base, name='Base'),
     path('logout/', LogoutView.as_view(next_page='/login/'), name='Logout'),
+    path('aseo/', aseo, name='aseo'),
+    path('computacion/', computacion, name='computacion'),
+    path('profesores/', profesores, name='profesores'),
 ]
--- Core/views.py
+++ Core/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
-
-
+from .views import CustomLoginView, Base,aseo,computacion,profesores
+def aseo(request):
+    return render(request, 'Core/aseo.html')
+def computacion(request):
+    return render(request, 'Core/computacion.html')
+def profesores(request):
+    return render(request, 'Core/profesores.html')
 def Base(request):
     return render(request, 'Core/Base.html')
 
