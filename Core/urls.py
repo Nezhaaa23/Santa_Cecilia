a69cf8bddf546646c161b7d63a7ccaea114b7b57
Nezha <esdeath19@gmail.com>
2024-11-25 23:22:34 -0300

    Boton Cerrar sesion 1.1

--- Core/urls.py
+++ Core/urls.py
@@ -7,7 +7,5 @@ urlpatterns = [
     path('', lambda request: redirect('login/')),  
     path('login/', CustomLoginView.as_view(), name='Login'),
     path('base/', Base, name='Base'),
-    
-    # Aseguramos que el next_page esté configurado correctamente
     path('logout/', LogoutView.as_view(next_page='/login/'), name='Logout'),
 ]
--- Core/views.py
+++ Core/views.py
@@ -2,12 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 
-# Vista base
+
 def Base(request):
     return render(request, 'Core/Base.html')
 
-# Clase para personalizar el formulario de login
 class CustomLoginView(LoginView):
-    template_name = 'Core/Login.html'  # Ruta del formulario de login
-    redirect_authenticated_user = True  # Si el usuario ya está autenticado, redirige a base
-    success_url = reverse_lazy('Base')  # Redirige a 'Base' después del login exitoso
+    template_name = 'Core/Login.html'  
+    redirect_authenticated_user = True  
+    success_url = reverse_lazy('Base')  
